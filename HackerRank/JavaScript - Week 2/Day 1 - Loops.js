ca0b1038d2c510b2987f66afa441ae88d6897ebc
Manraj Singh <manrajsinghgrover@gmail.com>
2016-03-02 21:36:39 +0530

    JavaScript Week 2 Started

---
+++ HackerRank/JavaScript - Week 2/Day 1 - Loops.js
@@ -0,0 +1,14 @@
+function vowelsAndConsonants(s) {
+    var vowels = ["a","e","i","o","u"];
+    for(var i = 0;i<s.length;i++){
+        if(vowels.indexOf(s[i])!=-1){
+            console.log(s[i]);
+        }
+    }
+    for(var i = 0;i<s.length;i++){
+        if(vowels.indexOf(s[i])!=-1){
+            continue;
+        }
+        console.log(s[i]);
+    }
+}
